6d2108dfb9fb3266393885455ff57ff35b15895b
Alice <spassos547@gmail.com>
2025-05-06 12:41:09 -0500

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,6 @@ set -gx PNPM_HOME "/home/alice/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+# pnpm end
 
 zoxide init --cmd cd fish | source
-# pnpm end
